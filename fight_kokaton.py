796fe90a9fc71a344c9a2e6f1fdb8da3050f3389
Sunager <c0b220829c@deu.teu.ac.jp>
2023-06-27 15:42:19 +0900

    練習2

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import time
+
 import pygame as pg
 
 
@@ -18,7 +19,7 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     if obj_rct.left < 0 or WIDTH < obj_rct.right:
         yoko = False
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
-        tate = False        
+        tate = False
     return yoko, tate
 
 
@@ -109,61 +110,61 @@ class Bomb:
 
 class Beam:
     """
-    ビームに関するクラス
+    こうかとんが放つビームに関するクラス
     """
     def __init__(self, bird: Bird):
         """
-        ビーム画像Surfaceを生成する
+        引数に基づきビームSurfaceを生成する
         引数 bird：ビームを放つこうかとん
         """
-        self.img = pg.image.load(f"ex03/fig/beam.png")
+        self.img = pg.transform.rotozoom(pg.image.load(f"ex03/fig/beam.png"), 0, 2.0)
         self.rct = self.img.get_rect()
-        self.rct.left=bird.rct.right 
-        self.rct.centery=bird.rct.centery
+        self.rct.left = bird.rct.right
+        self.rct.centery = bird.rct.centery
         self.vx, self.vy = +5, 0
-        
+    
     def update(self, screen: pg.Surface):
         """
         ビームを速度ベクトルself.vx, self.vyに基づき移動させる
         引数 screen：画面Surface
-        """    
+        """
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
 
-
 def main():
-    pg.display.set_caption("たたかえ！こうかとん" )
+    pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("ex03/fig/pg_bg.jpg")
     bird = Bird(3, (900, 400))
     bomb = Bomb((255, 0, 0), 10)
-    beam =None
+    beam = None
 
-    clock = pg.time.Clock()               
+    clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            if event.type==pg.KEYDOWN and event.key==pg.K_SPACE:
-                beam = Beam(bird)  # ビームクラスのインスタンスを作成
-
-        
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                beam = Beam(bird)  # ビームクラスのインスタンスを生成する
+                
         screen.blit(bg_img, [0, 0])
-
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
         
         if beam is not None and bomb is not None:
             if bomb.rct.colliderect(beam.rct):
-                bomb= None
-                beam=None
+                bomb = None
+                beam = None
+                bird.change_img(6, screen)
+                pg.display.update()              
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
@@ -173,7 +174,7 @@ def main():
             beam.update(screen)
         pg.display.update()
         tmr += 1
-        clock.tick(50)  
+        clock.tick(50)
 
 
 if __name__ == "__main__":
